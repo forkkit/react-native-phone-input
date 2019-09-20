c7ed355c010f63a87f277a80695843a7b1e12e58
Alexander Ewetumo <alex.e@gini.co>
2019-09-20 18:00:41 +0800

    UPT: Making props optional

--- index.d.ts
+++ index.d.ts
@@ -21,29 +21,29 @@ declare module 'react-native-phone-input' {
   }
   
   export interface Props extends React.Props<any> {
-    flagComponent: PropTypes.func,
-    textComponent: PropTypes.func,
-    initialCountry: PropTypes.string,
-    onChangePhoneNumber: PropTypes.func,
-    value: PropTypes.string,
-    style: StyleType,
-    flagStyle: StyleType,
-    textStyle: StyleType,
-    offset: PropTypes.number,
-    textProps: PropTypes.object,
-    onSelectCountry: PropTypes.func,
-    onPressCancel: PropTypes.func,
-    onPressConfirm: PropTypes.func,
-    pickerButtonColor: PropTypes.string,
-    pickerBackgroundColor: PropTypes.string,
-    pickerItemStyle: StyleType,
-    cancelText: PropTypes.string,
-    cancelTextStyle: StyleType,
-    confirmText: PropTypes.string,
-    confirmTextTextStyle: StyleType,
-    disabled: PropTypes.bool,
-    allowZeroAfterCountryCode: PropTypes.bool
-    countriesList: Array<Country>
+    flagComponent?: PropTypes.func,
+    textComponent?: PropTypes.func,
+    initialCountry?: PropTypes.string,
+    onChangePhoneNumber?: PropTypes.func,
+    value?: PropTypes.string,
+    style?: StyleType,
+    flagStyle?: StyleType,
+    textStyle?: StyleType,
+    offset?: PropTypes.number,
+    textProps?: PropTypes.object,
+    onSelectCountry?: PropTypes.func,
+    onPressCancel?: PropTypes.func,
+    onPressConfirm?: PropTypes.func,
+    pickerButtonColor?: PropTypes.string,
+    pickerBackgroundColor?: PropTypes.string,
+    pickerItemStyle?: StyleType,
+    cancelText?: PropTypes.string,
+    cancelTextStyle?: StyleType,
+    confirmText?: PropTypes.string,
+    confirmTextTextStyle?: StyleType,
+    disabled?: PropTypes.bool,
+    allowZeroAfterCountryCode?: PropTypes.bool
+    countriesList?: Array<Country>
   }
   
   export default class PhoneInput extends React.Component<Props, State> {
